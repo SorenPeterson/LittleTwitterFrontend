c974f2309bbefe5ad2b52c6b99764b7463b38169
Soren Peterson <soren468@gmail.com>
2015-03-03 15:12:31 -0800

    Displaying everything and we have a crazy button

--- application.js
+++ application.js
@@ -3,11 +3,28 @@ var displayHomePage = function() {
   request.open("GET", 'http://localhost:3000');
   request.onload = function() {
     var response = JSON.parse(this.response);
-    console.log(response)
-    console.log(response.most_popular_hashtags);
-    console.log(response.)
+    displayHashtags(response.most_popular_hashtags);
+    displayTweets(response.most_recent_tweets);
   }
   request.send();
+}
+
+var displayHashtags = function(response) {
+  var display = document.getElementById("hashtag-display");
+  response.forEach(function(hashtag) {
+    var listItem = document.createElement('li');
+    listItem.innerHTML = hashtag.hashtag
+    display.appendChild(listItem);
+  });
+}
+
+var displayTweets = function(response) {
+  var display = document.getElementById("tweet-display");
+  response.forEach(function(tweet) {
+    var listItem = document.createElement('li');
+    listItem.innerHTML = tweet.tweet
+    display.appendChild(listItem);
+  });
 }
 
 displayHomePage();
